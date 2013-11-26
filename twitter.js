1ee837289509eb2dea1b7e57b0f8c89d8d2788db
lealbaugh <l.e.albaugh@gmail.com>
2013-11-26 12:38:20 -0500

    Adds ability to retweet.  Remember that tweet ids should ideally be passed around as strings.

--- twitter.js
+++ twitter.js
@@ -1,5 +1,6 @@
 var twitter = require('ntwitter');
 var keys = require("./apikeys.js");
+var screen_name = "gnurr";
 
 var twit = new twitter({
 	consumer_key: keys.consumer_key,
@@ -8,11 +9,40 @@ var twit = new twitter({
 	access_token_secret: keys.access_token_secret
 });
 
-twit.stream('statuses/filter', {follow:'107145739'}, function(stream){
-	console.log("making a stream");
-	stream.on('data', function (data){
-		if (data.text){
-			console.log(pluck(data, ["text"]));
-		}	
+
+function openStream(user_id){
+	twit.stream('statuses/filter', {follow:user_id}, function(stream){
+		console.log("making a stream for user ", user_id);
+		stream.on('data', function (data){
+			if (data.id_str){
+				id = data.id_str				
+				twit.retweetStatus(id, function(data){
+					console.log("retweeted!");
+				});
+			}	
+		});
 	});
-});
+}
+
+
+function initiateStream(screen_name) {
+	twit.get("/users/show.json", {"screen_name":screen_name}, function(fake, data) {
+		console.log("USER ID: ",data.id);
+		openStream(data.id);
+	});
+};
+
+
+function pluck(tweet, keys){
+	return keys.reduce(function(a, k){
+		a[k] = tweet[k];
+		return a;
+	}, {});
+}
+
+// Make it go!
+initiateStream(screen_name);
+
+// twit.updateStatus("Hello?", null, function(){
+// 	console.log("tweeted");
+// })
